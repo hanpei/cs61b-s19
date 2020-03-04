d699a93805fefad7494820c9898cd4891664e04e
hanpei <hanpei@126.com>
2020-03-04 21:31:46 +0800

    lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,6 +82,9 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
+        if (A == null) return B;
+        if (B == null) return A;
+
         IntList p = A;
         while (A.rest != null) {
             A = A.rest;
